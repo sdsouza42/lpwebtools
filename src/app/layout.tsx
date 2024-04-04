a4382c0a590adfbb3a8a8a2f3f479efdd2b8843b
sdsouza <dsouza.savio@hotmail.com>
2024-04-03 18:15:19 -0700

    new backend

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/UI/global.css';
+import { inter } from '@/app/UI/fonts';
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    );
+  }
