cf17c4f42c1df3adae19d598a5a836989e8d4fa1
sdsouza <dsouza.savio@hotmail.com>
2024-04-27 08:20:52 -0700

    desktop front page done, fix mobile

---
+++ src/app/Stats/page.tsx
@@ -0,0 +1,10 @@
+// import styles from '@/app/UI/home.css';
+export default function Page() {
+    return (
+        <div>
+            <p>Load stats here</p>
+        </div>
+
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,36 @@
-// import styles from '@/app/UI/home.css';
-export default function Page() {
-    return (
-    <main className="min-h-screen p-6 bg-gray-200"> 
-        <h1>Home page</h1>
-        <div className="w-3/4 mx-auto mt-0 bg-white shadow-xl border-2 border-blue-500 rounded-md">
-          <div className=" mx-autorelative flex flex-row items-center justify-around">
-            <div className="relative inline-block text-left">
-              <button type="button" 
-              className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Class: classname</button>
-              </div>
-            <div className="relative inline-block text-left">
-                <div>
-                  <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-haspopup="true" aria-expanded="true">
-                    Level: classlevel
-                    <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                      <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                    </svg>
-                  </button>           
-                </div>   
-              </div>
+import styles from '@/app/UI/home.css'; 
+import {Button} from '@/app/UI/button'; 
+export default function Page() { 
+  return ( 
+  <main className="static min-h-screen p-6 bg-gray-200 flex flex-col items-center justify-center">
+    <h1>Home page</h1>
+    <div className=" mx-autorelative flex flex-row items-center justify-around" id='lvlbar'>
+      <Button className=""> Class: classname </Button>
+      <Button className=""> Level: classlevel </Button>
+    </div>
+    <div className="w-3/4 mx-auto mt-0 bg-white shadow-xl border-2 border-blue-500 rounded-md mx-autorelative flex justify-start" id='outterframe'>
+      <div className=" mx-autorelative items-center justify-around" id='sidebar'>
+        <Button className=""> Skills </Button>
+        <Button className=""> Passives </Button>
+        <Button className=""> Equipment </Button>
+        <Button className=""> Idols </Button>
+        <Button className=""> Blessing </Button>
+      </div>
+      <div className="mx-autorelative flex flex-col justify-end flex-grow content-center" id='innerframe'>
+        <div className="flex-grow" id='dcontent'>
+          <div>
+            <p>content</p>
           </div>
-          <div className=" mx-autorelative flex flex-row items-center justify-around">
-            <div className='flex flex-col items-center justify-around'>
-              <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Skills </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Passives </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Equipment </button>
-            </div>
-            <div className= 'Main Content'>
-              <div>
-                <p>content</p>
-              </div>
-            </div>
-            <div className = 'flex flex-col items-center justify-around'>
-            <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Blessing </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Idols </button>
-            </div>
-          </div>
-          <div className=" mx-autorelative flex flex-row items-center justify-around">
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Skill1 </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Skill2 </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Skill3 </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Skill4 </button>
-                <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                Skill5 </button>
-          </div>
-        </div>        
-      </main>
-
-    );
-  }
-  
+        </div>
+        <div className=" mx-autorelative flex flex-row justify-around" id='skillbar'>
+          <Button className=""> Skill1 </Button>
+          <Button className=""> Skill2 </Button>
+          <Button className=""> Skill3 </Button>
+          <Button className=""> Skill4 </Button>
+          <Button className=""> Skill5 </Button>
+        </div>
+      </div>
+    </div>
+  </main> 
+  ); 
+}
